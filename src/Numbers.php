e204c4e4b2cefb24c9efead91d53a70d451dcb36
Sam Mousa <sam@mousa.nl>
2016-07-19 12:39:54 +0000

    Changed the compression class to not be static.

--- src/Numbers.php
+++ src/Numbers.php
@@ -6,5 +6,81 @@ namespace SamIT\Compress;
 
 class Numbers
 {
+    /**
+     * Compresses an array of numbers, keys and ordering are ignored.
+     * @param int[] $numbers
+     * @return string A string containing the compressed numbers.
+     */
+    public function compress(array $numbers)
+    {
+        sort($numbers);
+        $rs = [];
+        $r = [];
+        $p = $numbers[0] - 1;
+        // Step 1: combine consecutive numbers into ranges.
+        foreach ($numbers as $c) {
+            if (!is_int($c)) {
+                throw new \InvalidArgumentException("All elements must be integers.");
+            }
+            if ($c - 1 === $p) {
+                $r[] = $c;
+            } else {
+                $rs[] = $r;
+                $r = [$c];
+            }
+            $p = $c;
+        }
+        $rs[] = $r;
 
+        $c = 0;
+        $prevC = 0;
+        $result = "";
+        // Step 2: Prefix set of numbers with a multiplier.
+        foreach ($rs as $r) {
+            while ($r[0] >= ($c + 1) * 100) {
+                $c++;
+            }
+            if ($c != $prevC) {
+                $result .= "$c|";
+            }
+
+            $result .= ($r[0] - $c * 100);
+            if (isset($r[1])) {
+                $result .= "-" . (end($r) - $c * 100);
+            }
+            $result .= ",";
+            $prevC = $c;
+        }
+        $result = substr($result, 0, -1);
+
+        return $result;
+    }
+
+    /**
+     *
+     * @param string $string The string containing compressed numbers.
+     * @return int[] The numbers obtained from decompressing the string.
+     */
+    public function decompress($string)
+    {
+        // Expand a again.
+        $expanded = [];
+        $c = 0;
+        foreach(explode(',', $string) as $range) {
+            $parts = explode('|', $range);
+            if (count($parts) === 2) {
+                $c = $parts[0];
+                $range = $parts[1];
+            }
+            $parts = explode('-', $range);
+            $expanded[] = $c * 100 + $parts[0];
+            if (count($parts) === 2) {
+                for($i = $parts[0] + 1; $i <= $parts[1]; $i++) {
+                    $expanded[] = $c * 100 + $i;
+                }
+            }
+
+        }
+        return $expanded;
+    }
 }
--- tests/NumbersTest.php
+++ tests/NumbersTest.php
@@ -4,7 +4,40 @@
 namespace SamIT\Compress\Tests;
 
 
+use SamIT\Compress\Numbers;
+
 class NumbersTest extends \PHPUnit_Framework_TestCase
 {
+    public function arrayProvider()
+    {
+        $count = 1000;
+        $maxLength = 10000;
+        $domainFactor = 10;
+        $result = [];
+        for ($i = 0; $i < $count; $i++) {
+            $length = rand(1, $maxLength);
+            $arr = [];
+            for($j = 0; $j < $length; $j++) {
+                $arr[] = rand(1, $domainFactor * $length);
+            }
+            $result[] = [$arr];
 
+        }
+        return $result;
+    }
+    /**
+     *
+     * @param $input
+     * @dataProvider arrayProvider
+     */
+    public function testSymmetry(array $input)
+    {
+        sort($input);
+        $compressor = new Numbers();
+        $compressed = $compressor->compress($input);
+        
+        $this->assertEquals($input, $compressor->decompress($compressed));
+
+        $this->assertLessThan(0.4, strlen($compressed) * 1.0 / strlen(serialize($input)), $compressed);
+    }
 }
